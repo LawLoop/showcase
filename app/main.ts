287120a1530fca8609c26cbd383f2e1904c11fe1
tblanchard <tblanchard@mac.com>
2016-10-27 12:55:20 -0700

    navigation

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,7 @@
-/*
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic
-      .platformBrowserDynamic()
-      .bootstrapModule(app.AppModule);
-  });
-})(window.app || (window.app = {}));
-*/
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+
 const platform = platformBrowserDynamic();
+
 platform.bootstrapModule(AppModule);
