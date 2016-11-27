965df03599321f08045e2c014c8bd2a565e0a9d1
tblanchard <tblanchard@mac.com>
2016-11-27 14:08:30 -0800

    updated server base url

--- dav/server.php
+++ dav/server.php
@@ -19,7 +19,7 @@ $server = new DAV\Server($rootDirectory);
 
 // If your server is not on your webroot, make sure the following line has the
 // correct information
-$server->setBaseUri('/api/server.php');
+$server->setBaseUri('/dav/server.php');
 
 // The lock manager is reponsible for making sure users don't overwrite
 // each others changes.
