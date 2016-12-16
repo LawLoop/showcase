fd61f3b0fff9f247ee48bb2107e91d33cdae5c81
tblanchard <tblanchard@mac.com>
2016-12-16 13:19:15 -0800

    added missing files

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+date_default_timezone_set('UTC');
+
+// The autoloader
+require_once '../api/vendor/autoload.php';
+
+use Sabre\DAV;
+
+$path = '/efs_local/dav/';
+if(isset($_REQUEST['project']))
+{
+    $path .= 'Projects/'.$_REQUEST['project'];
+}
+
+// Now we're creating a whole bunch of objects
+$rootDirectory = new \Sabre\DAV\FSExt\Directory($path);
+
+// The server object is responsible for making sense out of the WebDAV protocol
+$server = new DAV\Server($rootDirectory);
+
+// If your server is not on your webroot, make sure the following line has the
+// correct information
+//$server->setBaseUri('/dav/');
+
+// The lock manager is reponsible for making sure users don't overwrite
+// each others changes.
+$lockBackend = new DAV\Locks\Backend\File('/efs_local/locks');
+$lockPlugin = new DAV\Locks\Plugin($lockBackend);
+$server->addPlugin($lockPlugin);
+
+// This ensures that we get a pretty index in the browser, but it is
+// optional.
+$server->addPlugin(new DAV\Browser\Plugin());
+
+// All we need to do now, is to fire up the server
+$server->exec();
+
+?>
